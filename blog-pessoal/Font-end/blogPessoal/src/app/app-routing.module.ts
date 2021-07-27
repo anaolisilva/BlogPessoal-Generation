381a8c71e558299fe9cc80686cb2ca46845b8cfb
Ana Oliveira <vianello.ana@gmail.com>
2021-07-27 10:11:37 -0300

    Adicionando rotas

--- blog-pessoal/Font-end/blogPessoal/src/app/app-routing.module.ts
+++ blog-pessoal/Font-end/blogPessoal/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { LogarComponent } from './logar/logar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo:'logar', pathMatch:'full'},
+  {path:'logar', component: LogarComponent},
+  {path:'cadastrar', component: CadastrarComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
